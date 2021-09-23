9d3caf5e0c340ae870a91ac3010b1eef6d7f95ca
= <dmsburn@gmail.com>
2021-09-23 06:56:31 -0300

    Ajustando puma no production

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 environment "production"
 
-bind  "unix:////home/ubuntu/apps/vacinaja/shared/tmp/sockets/puma.sock"
+bind  "unix:///home/ubuntu/apps/vacinaja/shared/tmp/sockets/puma.sock"
 pidfile "/home/ubuntu/apps/vacinaja/shared/tmp/pids/puma.pid"
 state_path "/home/ubuntu/apps/vacinaja/shared/tmp/sockets/puma.state"
 directory "/home/ubuntu/apps/vacinaja/current"
@@ -10,6 +10,6 @@ threads 1,2
 
 daemonize true
 
-activate_control_app 'unix:///home/ubuntu/apps/vacinaja//shared/tmp/sockets/pumactl.sock'
+activate_control_app 'unix:///home/ubuntu/apps/vacinaja/shared/tmp/sockets/pumactl.sock'
 
 prune_bundler
