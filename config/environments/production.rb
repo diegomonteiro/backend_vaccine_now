5298af0f5f293923c47fae267bea9aa4677a0a97
= <dmsburn@gmail.com>
2021-09-28 13:59:38 -0300

    Ajustando o webpacer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,8 @@ Rails.application.configure do
   config.require_master_key = false
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   config.secret_key_base = "teste"
 
