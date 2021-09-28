56c2cc6b167f91a14ecafd180ad5384545fc568a
= <dmsburn@gmail.com>
2021-09-28 20:08:56 -0300

    Ajustando ambiente de desenvolvimento

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: '3.137.194.200', port: 3000 }
 
   #config.action_mailer.delivery_method = :letter_opener
-  #config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
