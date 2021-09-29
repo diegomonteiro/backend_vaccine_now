d3d5b8395c409c13e95285ca69bc2e89f9024683
= <dmsburn@gmail.com>
2021-09-28 21:01:30 -0300

    Ajustando para ambiente de produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: '3.137.194.200', protocal: 'https', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://3.137.194.200', port: 3000 }
 
   #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
