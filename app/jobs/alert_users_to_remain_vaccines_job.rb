cfc079b12328e2ac8b8951d39e8ad24854bcecca
= <dmsburn@gmail.com>
2022-11-06 11:10:57 -0300

    Corrigindo parametros de tradução

--- app/jobs/alert_users_to_remain_vaccines_job.rb
+++ app/jobs/alert_users_to_remain_vaccines_job.rb
@@ -5,17 +5,19 @@ class AlertUsersToRemainVaccinesJob < ApplicationJob
     
     VaccinationPoint.all.each do |v|
       
-      #vv = Vaccination.where("vaccination_point_id = ? and updated_at >= current_date", v.id).sum(:remain_doses)
-
       vv = Vaccination.select("vaccine_types.name as vaccine_type, diseases.name as disease, date_trunc('day',vaccinations.updated_at) as data, sum(vaccinations.remain_doses) as remain_doses, sum(vaccinations.discarded_doses) as discarded_doses").where("vaccination_point_id = ?", v.id).joins(:vaccine_type => :disease).group("vaccine_types.id, diseases.id, data")
 
+      #Select users in radius of Vaccination Point
       up = UserPosition.within(v.radius_km, :origin => v, :units => :kms)
+      
+      #Select uniq users in selection
       users_ids = up.pluck(:user_id).uniq
 
-      #puts "Vaccination Point: #{v.name} - Closest Users: #{users_ids.count} - Vaccines: #{vv}"
+      puts "Vaccination Point: #{v.name} - Closest Users: #{users_ids.count} - Vaccines: #{vv}"
       
       users = User.includes(:roles).where("id in (?)",users_ids)
       users.each do |user|
+        #Send email only patient role users
         if user.has_role? :patient
           UserMailer.with(user: user, vaccination_point: v, vaccinations: vv).notificar_usuarios_por_vacinas.deliver_now
         end
